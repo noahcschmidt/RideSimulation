adcaa1a64f4d830d9967406d72b07ac95d17a00d
noahcschmidt <35358649+noahcschmidt@users.noreply.github.com>
2020-02-12 15:15:49 -0800

    Start Code Party

--- src/main/java/Party.java
+++ src/main/java/Party.java
@@ -1,9 +1,23 @@
 public class Party {
     private int numPeople;
+    private double weight;
+    private boolean isHandicapped;
+
+    public Party(int numPeople, double weight, boolean isHandicapped) {
+        this.numPeople = numPeople;
+        //TODO Finish
+
+    }
 
     public int getPeople() {
         return numPeople;
     }
+
+    public void setPeople(int numPeople) {
+        //TODO FINISH
+    }
+
+    //Write other getters / setters
 
 
 }
