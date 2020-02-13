f8bd19f35a45941e9416c6fa942d7a80ab2947d9
noahcschmidt <35358649+noahcschmidt@users.noreply.github.com>
2020-02-13 15:33:34 -0800

    Renamed Test Files

--- src/main/java/Party.java
+++ src/main/java/Party.java
@@ -1,23 +1,2 @@
 public class Party {
-    private int numPeople;
-    private double weight;
-    private boolean isHandicapped;
-
-    public Party(int numPeople, double weight, boolean isHandicapped) {
-        this.numPeople = numPeople;
-        //TODO Finish
-
-    }
-
-    public int getPeople() {
-        return numPeople;
-    }
-
-    public void setPeople(int numPeople) {
-        //TODO FINISH
-    }
-
-    //Write other getters / setters
-
-
 }
