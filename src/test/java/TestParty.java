1214392132db4d7726dbe15ff1d3150d4e90af8c
noahcschmidt <35358649+noahcschmidt@users.noreply.github.com>
2020-02-13 15:51:01 -0800

    Added JUnit Test

--- src/test/java/TestParty.java
+++ src/test/java/TestParty.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+import org.junit.Assert;
+
 public class TestParty {
+
+    @Test
+    public void testConstruct() {
+        Party p1 = new Party(4);
+        Assert.assertEquals()
+
+
+
+    }
 }
