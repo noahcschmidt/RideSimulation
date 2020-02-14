3cdc1efae2e9e5b5af705f7ee4d4a9a49fe375ff
noahcschmidt <35358649+noahcschmidt@users.noreply.github.com>
2020-02-13 16:28:46 -0800

    Party Test File General Written

--- src/test/java/TestParty.java
+++ src/test/java/TestParty.java
@@ -1,14 +1,46 @@
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class TestParty {
 
     @Test
     public void testConstruct() {
         Party p1 = new Party(4);
-        Assert.assertEquals()
+        Assert.assertEquals(p1.getNumPeople(), 4);
+        Assert.assertEquals(p1.getWeight(), 137*p1.getNumPeople());
 
+        Party p2 = new Party(9, 400.56);
+        Assert.assertEquals(p2.getNumPeople(), 9);
+        Assert.assertEquals(p2.getWeight(), 400.56);
+        Assert.assertFalse(p2.isHandicapped());
 
+        Party p3 = new Party(3, 250.6, true);
+        Assert.assertEquals(p3.getNumPeople(), 3);
+        Assert.assertEquals(p3.getWeight(), 250.6);
+        Assert.assertTrue(p3.isHandicapped());
 
     }
+
+    @Test
+    public void testIllegalNumPeople() {
+        try {
+            Party p1 = new Party(0);
+            Party p2 = new Party(-5);
+            Assert.fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //Error caught, success
+        }
+    }
+
+    @Test
+    public void testIllegalWeight() {
+        try {
+            Party p1 = new Party(2, 0);
+            Party p2 = new Party(5, -12.8)
+            Assert.fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //error caught, success
+        }
+    }
+
 }
