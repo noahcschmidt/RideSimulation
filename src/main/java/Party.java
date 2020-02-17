7006180830896e48ecc281fbe9907bb23c00a362
Zhuoyun Chen <zhuoyun.chen@calbaptist.edu>
2020-02-17 14:53:29 -0800

    Finished Party Class

--- src/main/java/Party.java
+++ src/main/java/Party.java
@@ -9,20 +9,43 @@ public class Party {
 
     public Party(int numPeople, double weight, boolean isHandicapped) {
         this.numPeople = numPeople;
+        this.weight = weight;
+        handicapped = isHandicapped;
+    }
 
+    public Party(int numPeople) {
+        this.numPeople = numPeople;
+    }
 
+    public Party(int numPeople, double weight) {
+        this.numPeople = numPeople;
+        this.weight = weight;
     }
 
     public int getNumPeople() {
         return numPeople;
     }
 
-    public void setPeople(int numPeople) {
-
+    public void setNumPeople(int numPeople) {
+        this.numPeople = numPeople;
     }
 
     //Write other getters / setters
+    public double getWeight() {
+        return weight;
+    }
 
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
 
+    public boolean isHandicapped(){
+        return handicapped;
+    }
+    public void setHandicapped(boolean isHandicapped){
+        handicapped = isHandicapped;
+    }
 }
+
+
 
