9afc476b9ed1dc82bb37333a7f356310e19e9c14
joshi <Jocelyne.Marquez@calbaptist.edu>
2020-02-26 15:27:59 -0800

    Line code

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -7,54 +7,52 @@ public class Line {
     private Queue<Integer> line;
     private int current;
 
-    public Line(List<Party> list1) {
-
-    }
-
-
-    //put the party number into a queue
-    public void add(Party p1) {
-        line = new LinkedList<>();
-        line.add();
-    }
 
     //get the length
     public int length() {
-        if(line.isEmpty()){
+        if (line.isEmpty()) {
             return 0;
         }
         return length();
     }
 
-    //remove current party and make next party the current
-    public void remove(Party p1){
-
-
+    //add to the line
+    public void addTo(){
+        if(hasNext() == true){
+            line.add(current);
+        }
     }
 
+
     //remove complete line
-    public void emptyLine(){
+    public void emptyLine() {
         line.removeAll(line);
     }
 
     //only to check if there is a next party
-    public  boolean hasNext(){
-        if(line.size() != 0){
+    public boolean hasNext() {
+        if (line.size() != 0) {
             return true;
         }
         return false;
     }
 
     //next party but not removing
-    public int peek(){
-        if(line.isEmpty()){
+    public int peek() {
+        if (line.isEmpty()) {
             return 0;
         } else {
             return line.peek();
         }
-
     }
 
+    //remove current party and make next party the current
+    public void remove(){
+        if(!line.isEmpty()){
+            current = 0;
+            line.remove(current);
+            current++;
+        }
+    }
 
 }
-
