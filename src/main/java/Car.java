a314cd64fd62167c3fadd12a4ff4640299d6a473
noahcschmidt <35358649+noahcschmidt@users.noreply.github.com>
2020-03-04 15:17:41 -0800

    Car Started, not finished

---
+++ src/main/java/Car.java
@@ -0,0 +1,95 @@
+import java.util.LinkedList;
+
+public class Car {
+    public static final int SEATS = 12;
+    public static final int BASE_WEIGHT = 300;
+
+    private double weight;
+    private double speed;
+    private int number;
+    private LinkedList<Party> parties;
+
+    public Car(int number, LinkedList<Party> parties) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Car number cannot be negative.");
+        }
+
+        this.speed = 0;
+        this.number = number;
+
+        if (parties == null) {
+            System.out.println("List of parties cannot be null. Creating list...");
+            this.parties = new LinkedList<>();
+        }
+        else {
+            this.parties = parties;
+        }
+
+        setPartyWeights();
+        checkNumPeople();
+    }
+
+    public Car(int number) {
+        this(number, new LinkedList<>());
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public int getNumPeople() {
+        int total = 0;
+        for (Party p : parties) {
+            total += p.getNumPeople();
+        }
+        return total;
+    }
+
+    public void addParty(Party p) {
+        if(getNumPeople() + p.getNumPeople() <= SEATS) {
+            parties.add(p);
+        }
+        else {
+            //TODO: error? IDK yet
+        }
+    }
+
+    /**
+     * Private helper method, calculates and sets weight based on base weight and weights of parties.
+     */
+    private void setPartyWeights() {
+        weight = BASE_WEIGHT;
+        for(Party p : parties) {
+            weight += p.getWeight();
+        }
+    }
+
+    /**
+     * Private helper, ensures the list of parties added to a car does not exceed the maximum amount of people.
+     */
+    private void checkNumPeople() {
+        int total = 0;
+        for (Party p : parties) {
+            total += p.getNumPeople();
+        }
+        if (total > SEATS) {
+            throw new IllegalArgumentException("List of parties cannot exceed number of seats: " + SEATS);
+        }
+    }
+}
