810a649d95c296f88c23dd1b9dfa3cb8cc1383b0
Zhuoyun Chen <zhuoyun.chen@calbaptist.edu>
2020-03-15 19:40:36 -0700

    track piece

--- src/main/java/TrackPiece.java
+++ src/main/java/TrackPiece.java
@@ -1,17 +1,17 @@
 public class TrackPiece {
     private double length;
     private int  difficulty;
-    private TrackPiece nextTrack;
+    private int nextTrack;
     private int car;
 
-    public TrackPiece(double length, int difficulty, TrackPiece nextTrack, int car){
+    public TrackPiece(double length, int difficulty, int nextTrack, int car){
         if(length <=0 ){
             throw new IllegalArgumentException("The length must be greater than 0!");
         }
         if(difficulty <= 0){
             throw new IllegalArgumentException("The difficulty must be greater than 0!");
         }
-        if(nextTrack == null){
+        if(nextTrack <= 0){
             throw new IllegalArgumentException("The nextTrack must be greater than 0!");
         }
         if(car <= 0){
@@ -33,8 +33,8 @@ public class TrackPiece {
         this.difficulty = difficulty;
     }
 
-    public TrackPiece getNextTrack(){return nextTrack;}
-    public void setNextTrack(TrackPiece nextTrack){
+    public int getNextTrack(){return nextTrack;}
+    public void setNextTrack(int nextTrack){
         this.nextTrack = nextTrack;
     }
 
